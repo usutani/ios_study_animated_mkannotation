da112d7a3ffeecfabc7bcb48f748219946e611a7
Yasuhiro Usutani <y_usutani@mac.com>
2020-02-13 11:05:12 +0900

    地図の表示

--- ios_study_animated_mkannotation/ViewController.swift
+++ ios_study_animated_mkannotation/ViewController.swift
@@ -7,14 +7,21 @@
 //
 
 import UIKit
+import MapKit
 
 class ViewController: UIViewController {
-
+    
+    //MARK: Constants
+    let LOC_COORD_KOBE_CITY_HALL = CLLocationCoordinate2D(latitude: 34.689486, longitude: 135.195739)
+    let COORD_SPAN = MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1)
+    
+    //MARK: Properties
+    @IBOutlet weak var mapView: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // 神戸市役所を中心に地図を表示する
+        mapView.region = MKCoordinateRegion(center: LOC_COORD_KOBE_CITY_HALL, span: COORD_SPAN)
     }
-
-
 }
-
