4f9bc779a1e2572b2c445ad56d3e94e98ba5b737
Yasuhiro Usutani <y_usutani@mac.com>
2020-02-16 16:14:20 +0900

    地図の中心にアノテーションを移動する

--- ios_study_animated_mkannotation/ViewController.swift
+++ ios_study_animated_mkannotation/ViewController.swift
@@ -11,26 +11,37 @@ import MapKit
 
 class ViewController: UIViewController, MKMapViewDelegate {
     
+    class TreasureHunterAnnotation : MKPointAnnotation {}
+    
     //MARK: Constants
     let LOC_COORD_KOBE_CITY_HALL = CLLocationCoordinate2D(latitude: 34.689486, longitude: 135.195739)
     let COORD_SPAN = MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1)
     
     //MARK: Properties
     @IBOutlet weak var mapView: MKMapView!
+    var treasureHunterAnnotation: TreasureHunterAnnotation!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // アノテーションの座標を神戸市役所に設定する
-        let a = MKPointAnnotation()
-        a.coordinate = LOC_COORD_KOBE_CITY_HALL
-        mapView.addAnnotation(a)
+        treasureHunterAnnotation = TreasureHunterAnnotation()
+        treasureHunterAnnotation.coordinate = LOC_COORD_KOBE_CITY_HALL
+        mapView.addAnnotation(treasureHunterAnnotation)
         
         // 神戸市役所を中心に地図を表示する
         mapView.region = MKCoordinateRegion(center: LOC_COORD_KOBE_CITY_HALL, span: COORD_SPAN)
     }
     
     //MARK: MKMapViewDelegate
+    
+    func mapView(_ mapView: MKMapView, regionDidChangeAnimated animated: Bool) {
+        // 地図の中心にアノテーションを移動する
+        let destinationLocation = mapView.centerCoordinate
+        UIView.animate(withDuration: 0.2, animations: {
+            self.treasureHunterAnnotation.coordinate = destinationLocation
+        }, completion: nil)
+    }
     
     func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
         let reuseId = "TreasureHunter"
