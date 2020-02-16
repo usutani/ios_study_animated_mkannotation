43f83bfb9ff1b4fbba68edfef94fa89ff5b1d97c
Yasuhiro Usutani <y_usutani@mac.com>
2020-02-16 12:25:20 +0900

    Extract methods

--- ios_study_animated_mkannotation/TreasureHunterAnnotationView.swift
+++ ios_study_animated_mkannotation/TreasureHunterAnnotationView.swift
@@ -18,38 +18,48 @@ class TreasureHunterAnnotationView: MKAnnotationView {
     
     override init(annotation: MKAnnotation?, reuseIdentifier: String?) {
         super.init(annotation: annotation, reuseIdentifier: reuseIdentifier)
-        
-        // フレームサイズを適切な値に設定する
-        var myFrame = frame
-        myFrame.size.width = 48
-        myFrame.size.height = 48
-        frame = myFrame
-        // 不透過プロパティをNOに設定することで、地図コンテンツが、
-        // レンダリング対象外のビューの領域を透かして見えるようになる。
         isOpaque = false
-        
+        setFrameSize()
+        setWalker()
+        startAnimation()
+    }
+    
+    func setFrameSize() {
+        // フレームサイズを適切な値に設定する
+        frame.size.width = 48
+        frame.size.height = 48
+    }
+    
+    func setWalker() {
         walker = CALayer()
         // contentsScale = [UIScreen mainScreen].scale は特に必要ない
         // contentsGravity = kCAGravityResizeなので
-        walker.frame = CGRect(x: 0, y: 0, width: 48, height: 48)
+        walker.frame = CGRect(x: 0, y: 0, width: frame.size.width, height: frame.size.height)
         walker.contents = UIImage(named: "Patorash")?.cgImage
         walker.contentsRect = CGRect(x: 0, y: 0, width: 0.25, height: 0.25)
         
-        layer.cornerRadius = myFrame.size.width / 2
+        layer.cornerRadius = frame.size.width / 2
         layer.addSublayer(walker)
-        
+    }
+    
+    func startAnimation() {
         // アニメーションの設定
         let walkAnimation = CAKeyframeAnimation(keyPath: "contentsRect")
-        walkAnimation.values = [
-            NSValue(cgRect: CGRect(x: 0, y: 0, width: 0.25, height: 0.25)),
-            NSValue(cgRect: CGRect(x: 0, y: 0.25, width: 0.25, height: 0.25)),
-            NSValue(cgRect: CGRect(x: 0, y: 0.5, width: 0.25, height: 0.25)),
-            NSValue(cgRect: CGRect(x: 0, y: 0.75, width: 0.25, height: 0.25)),
-        ]
+        walkAnimation.values = aroundAnimationRectValues()
         walkAnimation.calculationMode = .discrete
         walkAnimation.duration = 1
         walkAnimation.repeatCount = Float.infinity
         walkAnimation.isRemovedOnCompletion = false
         walker.add(walkAnimation, forKey: "walk")
+    }
+    
+    func aroundAnimationRectValues() -> [NSValue] {
+        let quarter = 0.25
+        return [
+            NSValue(cgRect: CGRect(x: 0, y: 0.00, width: quarter, height: quarter)),
+            NSValue(cgRect: CGRect(x: 0, y: 0.25, width: quarter, height: quarter)),
+            NSValue(cgRect: CGRect(x: 0, y: 0.50, width: quarter, height: quarter)),
+            NSValue(cgRect: CGRect(x: 0, y: 0.75, width: quarter, height: quarter)),
+        ]
     }
 }
